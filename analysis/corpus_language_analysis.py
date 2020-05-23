383ab2e4865a28728a80b781774966f1a10ede2c
racine-p-a <pierrealexandreracine@gmail.com>
2020-05-23 18:19:37 +0200

    Old code removed.

--- analysis/corpus_language_analysis.py
+++ analysis/corpus_language_analysis.py
@@ -1,11 +1,17 @@
 # -*- coding:Utf-8 -*-
 
+
 class CorpusLanguageAnalysis(object):
     """
     Representation of a file and manages all computations ad operations required.
     """
-
     def __init__(self):
         """
+        At class call, we have to follow these steps :
+        - make a list of all folders in the language corpus
+        - check if a result file already exists :
+            - it exists -> we keep it
+            - it does not exist -> we analyse the texts inside the folder and create the file with the result
         """
+        # Get folder list
         print('yo')
